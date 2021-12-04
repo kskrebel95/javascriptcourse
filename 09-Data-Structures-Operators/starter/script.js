fadbf68bc79d3e5d457bf07f2972d90cb3dcde78
kskrebel95 <ramkissoon200@gmail.com>
2021-12-04 17:40:51 -0400

    up to for of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -248,3 +248,192 @@ add(...v);
 restaurant.orderPizza('cheese', 'tomato', 'pine', 'chicken', 'ham');
 
 //Short Circuiting
+
+//logical operators can take in any datatype and return any data type
+
+// OR operator
+//with the || operator the first turthy value is returned : 3
+console.log(3 || 'Jonsas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null); //will return null even though both are flasy values
+
+console.log(undefined || 0 || '' || 'Hello' || 'Jonas'); // Returns Hello the first truthy value(can work with any number of values)
+
+//Pratical use (setting default values if a property exists)
+
+//if a property on the resturant object (guests) does not exist, create a default of 10
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+
+// can also say
+const guests2 = restaurant.numGuests || 10;
+
+//NOTE :if a value is intended to be 0 it changes to 10 since 0 is falsy
+
+//Can be fixed with the NULLISH operator
+
+restaurant.numGuests = 0;
+
+//The ?? only checks for null or undefined. NOT 0 and ''
+const guests4 = restaurant.numGuests ?? 10;
+console.log(guests4);
+
+//AND operator
+
+// with the && operator the first falsy value is returned
+console.log(0 && 7);
+console.log(7 && 'Jonas'); // returns the scond truthy value
+console.log(null && undefined);
+
+console.log('Jonas' && 7 && null && 0);
+
+//if resturant.orderPizza exists then call the function with mushrooms and spinach
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+// is the same as
+
+//orderPizza exists and call it with mushrooms and spinach
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+// Coding challenge 1
+
+// We're building a football betting app (soccer for my American friends �)!
+// Suppose we get data from a web service about a certain game ('game' variable on
+// next page). In this challenge we're gonna work with that data.
+// Your tasks:
+
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+//  1.
+const [players1, players2] = game.players;
+
+// my solution
+
+// const [...players1] = game.players[0];
+// const [...players2] = game.players[1];
+
+console.log(players1, players2);
+
+// 2.
+const [gk, ...fieldPlayers] = game.players[0];
+console.log(gk, fieldPlayers);
+
+// 3.
+const allPlayers = [...players1, ...players2];
+
+//my solution
+// const allPlayers = [...game.players[0], ...game.players[1]];
+
+console.log(allPlayers);
+
+// 4.
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5.
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+//My solution
+// const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+//6.
+
+const printGoals = function (...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(`Goal scored by ${args[i]}`);
+  }
+  console.log(`Total goals: ${args.length}`);
+};
+
+printGoals(...game.scored);
+
+//7.
+
+team1 > team2 && console.log(`Team 1 is more likely to win`);
+team1 < team2 && console.log(`Team 2 is more likely to win`);
+
+//For of loop
+
+const menu1 = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu1) console.log(item);
+
+//menu1.enteries contain an array iterator containing every menu item with an array containing the index and value
+for (const item of menu1.entries()) {
+  console.log(`${item[0] + 1}:${item[1]} `);
+}
